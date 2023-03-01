b8677b627c605f05cba6ccf24dcab4dd8bbc140c
raj <raj@gmail.com>
2023-03-01 14:42:20 +0530

    create a dto and repository

---
+++ src/main/java/com/example/User/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.User.repository;
+
+import com.example.User.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
--- src/main/java/com/example/User/user/User.java
+++ src/main/java/com/example/User/user/User.java
@@ -1,10 +1,14 @@
 package com.example.User.user;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Table
 @Entity(name = "User_table")
 public class User {
